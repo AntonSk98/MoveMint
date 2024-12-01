d631aa9044e89d9d87d08608bf5b5a139a5742cd
ansk98 <anton.tech98@gmail.com>
2024-12-01 21:59:29 +0100

    Initial commit

---
+++ src/main/java/ansk98/de/movemintserver/MoveMintServerApplication.java
@@ -0,0 +1,13 @@
+package ansk98.de.movemintserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoveMintServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoveMintServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ansk98/de/movemintserver/MoveMintServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ansk98.de.movemintserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoveMintServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
